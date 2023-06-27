e712e66642ef5c872232eeca547935b512d173ee
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-06-27 21:56:19 +0300

    seminar 1_0

---
+++ lesson_1/1_0/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+Console.WriteLine("Write a number");
+string num_str = Console.ReadLine()!;
+
+int num = int.Parse(num_str);
+
+Console.WriteLine(num * num);
