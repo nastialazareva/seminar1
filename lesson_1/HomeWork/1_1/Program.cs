80b2fa317749620af249f05baec27cfc0c9752c4
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-06-28 00:07:16 +0300

    add HW 1_1

---
+++ lesson_1/HomeWork/1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int max = 0;
+
+if (a < b)
+{
+    a = b;
+}
+if (a < c)
+{
+    a = c;
+}
+
+Console.WriteLine("Максимум = " + a);
