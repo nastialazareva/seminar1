e6b025f6b7b2f5f0f37e3407d77bf2c2f32faba1
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-06-27 23:41:54 +0300

    add HW 1_0

---
+++ lesson_1/HomeWork/1_0/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+    Console.Write("Максимум= " + a);
+}
+else if (b > a)
+{
+    Console.Write("Максимум= " + b);
+}
+else
+    Console.Write("Числа равны между собой");
