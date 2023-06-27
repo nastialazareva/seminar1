d4cd053cb681ea992bcb16b3812f2bce736cbdee
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-06-27 22:22:15 +0300

    add lesson 1_1

---
+++ lesson_1/1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if (a / b == b)
+{
+    Console.Write("Квадрат");
+}
+else
+{
+    Console.Write("Не квадрат");
+}
