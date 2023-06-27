a03d0ee5a608ef1ed28ad08e94265da785d32649
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-06-27 22:49:26 +0300

    add lessom 1_2

---
+++ lesson_1/1_2/Program.cs
@@ -0,0 +1,34 @@
+﻿int a = int.Parse(Console.ReadLine())!;
+
+if (a == 1)
+{
+    Console.Write("Понедельник");
+}
+else if (a == 2)
+{
+    Console.Write("Вторник");
+}
+else if (a == 3)
+{
+    Console.Write("Среда");
+}
+else if (a == 4)
+{
+    Console.Write("Четверг");
+}
+else if (a == 5)
+{
+    Console.Write("Пятница");
+}
+else if (a == 6)
+{
+    Console.Write("Суббота");
+}
+else if (a == 7)
+{
+    Console.Write("Воскресенье");
+}
+else
+{
+    Console.Write("Неверное число");
+}
