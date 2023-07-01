e6b98fe8427fad295131ea1dbd2d6d0a1a3d3caa
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-07-01 20:39:07 +0300

    add lesson 2_0

---
+++ lesson_2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+int th_num = new Random().Next(100, 1000);
+int result = TakeNum(th_num);
+Console.WriteLine(result);
