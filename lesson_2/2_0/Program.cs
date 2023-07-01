ace7b9f6be07d5d9df0435c4efd19be3c3a67729
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-07-01 20:47:51 +0300

    add 2_0

--- lesson_2/2_0/Program.cs
+++ lesson_2/2_0/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+int th_num = new Random().Next(100, 1000);
+int result = TakeNum(th_num);
+Console.WriteLine(result);
