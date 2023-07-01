be772fb372c1b3dda8d234c51aa71c3d6191444c
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-07-01 21:59:58 +0300

    add HW 1_0

---
+++ lesson_2/HomeWork/HW 1_0/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int TakeNum(int num)
+{
+    return num / 10 % 10;
+}
+
+int newNum = int.Parse(Console.ReadLine()!);
+int result = TakeNum(newNum);
+Console.WriteLine(result);
