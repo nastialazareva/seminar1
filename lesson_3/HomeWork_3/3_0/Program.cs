ee00e67c5de034503668524f19dd489782ca2d92
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-07-05 00:38:52 +0300

    add HW 3_0

--- lesson_3/HomeWork_3/3_0/Program.cs
+++ lesson_3/HomeWork_3/3_0/Program.cs
@@ -4,3 +4,17 @@
 23432 -> да
 12821 -> да*/
 
+void FindSame(int num)
+{
+    int num12 = num / 1000;
+    int num4 = num / 10 % 10;
+    int num5 = num % 10;
+
+    if (num12 == num4 + num5 * 10)
+        Console.Write(num + " - это палиндром");
+    else
+        Console.Write(num + " - это не палиндром");
+}
+Console.Write("Введите число из 5 цифр: ");
+int bigNum = int.Parse(Console.ReadLine()!);
+FindSame(bigNum);
