02c4fc24873ef4d8264135b7612d78b3e3bcba80
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-07-11 19:47:55 +0300

    add 4_3

---
+++ lesson_4/4_3/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+4 -> 24
+5 -> 120 */
+
+int MultNum(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+Console.Write("Ведите число: ");
+int newNum = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Произведение чисел = " + MultNum(newNum));
