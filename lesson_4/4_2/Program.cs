00fcdfb427c9c9a608bd247b7764d28de54c0d59
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-07-11 19:33:46 +0300

    add 4_2

---
+++ lesson_4/4_2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+456 -> 3
+75 -> 2
+89126 -> */
+
+int sumNums(int num)
+{
+    int quantNum = 0;
+    for (int i = 1; num > 0; i++)
+    {
+        num /= 10;
+        quantNum = i;
+    }
+    return quantNum;
+}
+Console.Write("Введите число: ");
+int newNum = int.Parse(Console.ReadLine()!);
+int result = sumNums(newNum);
+Console.Write("Количество цифр в числе = " + result);
