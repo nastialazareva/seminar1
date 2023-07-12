eaea80de3b78c173b06154b22b6bc26750da9580
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-07-12 19:16:43 +0300

    add HW 4_1

---
+++ lesson_4/HW_4/4_1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напиши цикл, который принимает на вход два числа (A и B) и возводит A
+в натуральную степень B.
+3,5 -> 243
+2,4 -> 16 */
+
+double NumsMult(int num_1, int num_2)
+{
+    double result = 1;
+    int num_2_pos = Math.Abs(num_2);
+    for (int i = 1; i <= num_2_pos; i++)
+    {
+        if (num_2 > 0)
+            result = result * num_1;
+        else
+            result = result / num_1;
+    }
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Число равно = " + NumsMult(A, B));
