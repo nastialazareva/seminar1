22a85bab77b2c4378dce58e8c2aba57f765a8d60
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-07-16 19:23:13 +0300

    add 6_2

---
+++ lesson_6/6_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+void ConvBin(int num)
+{
+    string result = "";
+
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    Console.Write("Это число в двоичном виде = " + result);
+}
+
+Console.Write("Введите десятичное число: ");
+int newNum = int.Parse(Console.ReadLine()!);
+ConvBin(newNum);
